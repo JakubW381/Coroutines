a9344b4ef8fee4eafc75ca662116e979a424390c
JakubW381 <wolo.zapasowy@gmail.com>
2024-12-05 22:48:43 +0100

    first commit

--- app/src/main/java/com/example/services/MainActivity.kt
+++ app/src/main/java/com/example/services/MainActivity.kt
@@ -7,26 +7,17 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.Build
 import android.os.Bundle
-import android.widget.GridView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.ScrollableDefaults
-import androidx.compose.foundation.gestures.ScrollableState
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -34,11 +25,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import com.example.services.ui.theme.ServicesTheme
@@ -48,13 +36,17 @@ import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresPermission
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.material3.DividerDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.VerticalDivider
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        createNotificationChannel()
+//        createNotificationChannel()
         enableEdgeToEdge()
         setContent {
             ServicesTheme {
@@ -70,41 +62,41 @@ class MainActivity : ComponentActivity() {
         var someData by remember { mutableStateOf(emptyList<BookInfo>()) }
         val myServiceIntent = Intent(context,MyService::class.java)
 
-        var permission by remember { mutableStateOf(checkNotificationPermission()) }
-        val requestPermissionLauncher = rememberLauncherForActivityResult(
-            ActivityResultContracts.RequestPermission()
-        ) { isGranted ->
-            permission  = isGranted;
-            if(!isGranted){
-                Toast.makeText(
-                    this,
-                    "Brak zezwolenia na powiadomienia",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
+//        var permission by remember { mutableStateOf(checkNotificationPermission()) }
+//        val requestPermissionLauncher = rememberLauncherForActivityResult(
+//            ActivityResultContracts.RequestPermission()
+//        ) { isGranted ->
+//            permission  = isGranted;
+//            if(!isGranted){
+//                Toast.makeText(
+//                    this,
+//                    "Brak zezwolenia na powiadomienia",
+//                    Toast.LENGTH_SHORT
+//                ).show()
+//            }
+//        }
 
-        @RequiresPermission
-        fun sendNotification(bookInfo: BookInfo){
-            val builder = NotificationCompat.Builder(this,"default_channel")
-                .setContentTitle("Downloaded")
-                .setContentText("${bookInfo.title}")
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setSmallIcon(R.drawable.downloads)
-            with(NotificationManagerCompat.from(this)){
-                notify(1,builder.build())
-            }
-        }
+//        @RequiresPermission
+//        fun sendNotification(bookInfo: BookInfo){
+//            val builder = NotificationCompat.Builder(this,"default_channel")
+//                .setContentTitle("Downloaded")
+//                .setContentText("${bookInfo.title}")
+//                .setPriority(NotificationCompat.PRIORITY_HIGH)
+//                .setSmallIcon(R.drawable.downloads)
+//            with(NotificationManagerCompat.from(this)){
+//                notify(1,builder.build())
+//            }
+//        }
 
         DisposableEffect(Unit) {
             val receiver = MyBroadcastReceiver{ bookInfo ->
                 if (!(someData.contains(bookInfo))){
                     someData = someData.plus(bookInfo)
-                    if (permission){
-                        sendNotification(bookInfo)
-                    }else{
-                        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-                    }
+//                    if (permission){
+//                        sendNotification(bookInfo)
+//                    }else{
+//                        requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+//                    }
                 }
             }
             val  intentFilter = IntentFilter("com.example.services.DATA_DOWNLOADED")
@@ -125,7 +117,6 @@ class MainActivity : ComponentActivity() {
             Button(
                 onClick = {
                     context.startService(myServiceIntent)
-
                 }
             ) {
                 Text("Start download service")
@@ -145,31 +136,32 @@ class MainActivity : ComponentActivity() {
                         Text("${item.charCount}", modifier = Modifier.weight(0.2f),textAlign = TextAlign.Center)
                         Text(item.mostCommonWord, modifier = Modifier.weight(0.2f),textAlign = TextAlign.Center)
                     }
+                    HorizontalDivider(Modifier.fillMaxWidth(), thickness = 2.dp)
                 }
             }
         }
 
     }
 
-    private fun createNotificationChannel(){
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            val name = "Download Notification"
-            val descriptionText = "Download Notification Channel"
-            val importance = NotificationManager.IMPORTANCE_HIGH
-            val channel = NotificationChannel("default_channel",name,importance).apply {
-                description = descriptionText
-            }
-            val notificationManager : NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
-    private fun checkNotificationPermission() : Boolean{
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
-            ContextCompat.checkSelfPermission(this,
-                Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
-
-        }else{
-            true
-        }
-    }
+//    private fun createNotificationChannel(){
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+//            val name = "Download Notification"
+//            val descriptionText = "Download Notification Channel"
+//            val importance = NotificationManager.IMPORTANCE_HIGH
+//            val channel = NotificationChannel("default_channel",name,importance).apply {
+//                description = descriptionText
+//            }
+//            val notificationManager : NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//            notificationManager.createNotificationChannel(channel)
+//        }
+//    }
+//    private fun checkNotificationPermission() : Boolean{
+//        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+//            ContextCompat.checkSelfPermission(this,
+//                Manifest.permission.POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED
+//
+//        }else{
+//            true
+//        }
+//    }
 }
--- app/src/main/java/com/example/services/MyService.kt
+++ app/src/main/java/com/example/services/MyService.kt
@@ -1,35 +1,40 @@
 package com.example.services
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
 import android.app.Service
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Handler
 import android.os.IBinder
 import android.util.Log
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.annotation.RequiresPermission
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.key
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
-import androidx.core.content.ContextCompat
-import java.util.ArrayList
-import java.util.jar.Manifest
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.awaitAll
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.internal.trimSubstring
+import java.io.IOException
 import kotlin.random.Random
 
 
-class MyService : Service() {
-    private val handler = Handler()
 
-    private val downloadBooks = DATA_DOWNLOADED()
+
+class MyService : Service() {
+    //private val handler = Handler()
+    private val coroutineScope = CoroutineScope(Dispatchers.IO)
+    //private val downloadBooks = DATA_DOWNLOADED()
+    val stopWords = setOf("able","about","above","abroad","according","accordingly","across","actually","adj","after","afterwards","again","against","ago","ahead","ain't","all","allow","allows","almost","alone","along","alongside","already","also","although","always","am","amid","amidst","among","amongst","an","and","another","any","anybody","anyhow","anyone","anything","anyway","anyways","anywhere","apart","appear","appreciate","appropriate","are","aren't","around","as","a's","aside","ask","asking","associated","at","available","away","awfully","back","backward","backwards","be","became","because","become","becomes","becoming","been","before","beforehand","begin","behind","being","believe","below","beside","besides","best","better","between","beyond","both","brief","but","by","came","can","cannot","cant","can't","caption","cause","causes","certain","certainly","changes","clearly","c'mon","co","co.","com","come","comes","concerning","consequently","consider","considering","contain","containing","contains","corresponding","could","couldn't","course","c's","currently","dare","daren't","definitely","described","despite","did","didn't","different","directly","do","does","doesn't","doing","done","don't","down","downwards","during","each","edu","eg","eight","eighty","either","else","elsewhere","end","ending","enough","entirely","especially","et","etc","even","ever","evermore","every","everybody","everyone","everything","everywhere","ex","exactly","example","except","fairly","far","farther","few","fewer","fifth","first","five","followed","following","follows","for","forever","former","formerly","forth","forward","found","four","from","further","furthermore","get","gets","getting","given","gives","go","goes","going","gone","got","gotten","greetings","had","hadn't","half","happens","hardly","has","hasn't","have","haven't","having","he","he'd","he'll","hello","help","hence","her","here","hereafter","hereby","herein","here's","hereupon","hers","herself","he's","hi","him","himself","his","hither","hopefully","how","howbeit","however","hundred","i'd","ie","if","ignored","i'll","i'm","immediate","in","inasmuch","inc","inc.","indeed","indicate","indicated","indicates","inner","inside","insofar","instead","into","inward","is","isn't","it","it'd","it'll","its","it's","itself","i've","just","k","keep","keeps","kept","know","known","knows","last","lately","later","latter","latterly","least","less","lest","let","let's","like","liked","likely","likewise","little","look","looking","looks","low","lower","ltd","made","mainly","make","makes","many","may","maybe","mayn't","me","mean","meantime","meanwhile","merely","might","mightn't","mine","minus","miss","more","moreover","most","mostly","mr","mrs","much","must","mustn't","my","myself","name","namely","nd","near","nearly","necessary","need","needn't","needs","neither","never","neverf","neverless","nevertheless","new","next","nine","ninety","no","nobody","non","none","nonetheless","noone","no-one","nor","normally","not","nothing","notwithstanding","novel","now","nowhere","obviously","of","off","often","oh","ok","okay","old","on","once","one","ones","one's","only","onto","opposite","or","other","others","otherwise","ought","oughtn't","our","ours","ourselves","out","outside","over","overall","own","particular","particularly","past","per","perhaps","placed","please","plus","possible","presumably","probably","provided","provides","que","quite","qv","rather","rd","re","really","reasonably","recent","recently","regarding","regardless","regards","relatively","respectively","right","round","said","same","saw","say","saying","says","second","secondly","see","seeing","seem","seemed","seeming","seems","seen","self","selves","sensible","sent","serious","seriously","seven","several","shall","shan't","she","she'd","she'll","she's","should","shouldn't","since","six","so","some","somebody","someday","somehow","someone","something","sometime","sometimes","somewhat","somewhere","soon","sorry","specified","specify","specifying","still","sub","such","sup","sure","take","taken","taking","tell","tends","th","than","thank","thanks","thanx","that","that'll","thats","that's","that've","the","their","theirs","them","themselves","then","thence","there","thereafter","thereby","there'd","therefore","therein","there'll","there're","theres","there's","thereupon","there've","these","they","they'd","they'll","they're","they've","thing","things","think","third","thirty","this","thorough","thoroughly","those","though","three","through","throughout","thru","thus","till","to","together","too","took","toward","towards","tried","tries","truly","try","trying","t's","twice","two","un","under","underneath","undoing","unfortunately","unless","unlike","unlikely","until","unto","up","upon","upwards","us","use","used","useful","uses","using","usually","v","value","various","versus","very","via","viz","vs","want","wants","was","wasn't","way","we","we'd","welcome","well","we'll","went","were","we're","weren't","we've","what","whatever","what'll","what's","what've","when","whence","whenever","where","whereafter","whereas","whereby","wherein","where's","whereupon","wherever","whether","which","whichever","while","whilst","whither","who","who'd","whoever","whole","who'll","whom","whomever","who's","whose","why","will","willing","wish","with","within","without","wonder","won't","would","wouldn't","yes","yet","you","you'd","you'll","your","you're","yours","yourself","yourselves","you've","zero","a","how's","i","when's","why's","b","c","d","e","f","g","h","j","l","m","n","o","p","q","r","s","t","u","uucp","w","x","y","z","I","www","amount","bill","bottom","call","computer","con","couldnt","cry","de","describe","detail","due","eleven","empty","fifteen","fifty","fill","find","fire","forty","front","full","give","hasnt","herse","himse","interest","itse”","mill","move","myse”","part","put","show","side","sincere","sixty","system","ten","thick","thin","top","twelve","twenty","abst","accordance","act","added","adopted","affected","affecting","affects","ah","announce","anymore","apparently","approximately","aren","arent","arise","auth","beginning","beginnings","begins","biol","briefly","ca","date","ed","effect","et-al","ff","fix","gave","giving","heres","hes","hid","home","id","im","immediately","importance","important","index","information","invention","itd","keys","kg","km","largely","lets","line","'ll","means","mg","million","ml","mug","na","nay","necessarily","nos","noted","obtain","obtained","omitted","ord","owing","page","pages","poorly","possibly","potentially","pp","predominantly","present","previously","primarily","promptly","proud","quickly","ran","readily","ref","refs","related","research","resulted","resulting","results","run","sec","section","shed","shes","showed","shown","showns","shows","significant","significantly","similar","similarly","slightly","somethan","specifically","state","states","stop","strongly","substantially","successfully","sufficiently","suggest","thered","thereof","therere","thereto","theyd","theyre","thou","thoughh","thousand","throug","til","tip","ts","ups","usefully","usefulness","'ve","vol","vols","wed","whats","wheres","whim","whod","whos","widely","words","world","youd","youre")
+
+    val urls = arrayListOf(
+        "https://www.gutenberg.org/cache/epub/11/pg11.txt",
+        "https://www.gutenberg.org/cache/epub/84/pg84.txt",
+        "https://www.gutenberg.org/cache/epub/2701/pg2701.txt",
+        "https://www.gutenberg.org/cache/epub/1513/pg1513.txt",
+        "https://www.gutenberg.org/cache/epub/25344/pg25344.txt")
+
 
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
@@ -37,38 +42,67 @@ class MyService : Service() {
         download()
         return START_STICKY
     }
+    private suspend fun getBookHttp(url:String) : String = withContext(Dispatchers.IO){
+        val client = OkHttpClient()
+        val request = Request.Builder().url(url).build()
+        try {
+            client.newCall(request).execute().use { response ->
+                if (!response.isSuccessful) {
+                    throw IOException("error: ${response.code}")
+                }
+                response.body?.string().orEmpty()
+            }
+        }
+        catch (e: Exception){
+            "Exception: ${e.message}"
+        }
+    }
+    private suspend fun awaitForAll(urls: List<String>) : List<String>{
+        return coroutineScope{
+            urls.map{ url ->
+                async {
+                    getBookHttp(url)
+                }
+            }.awaitAll()
+        }
+    }
 
     private fun download(){
-            val pair = downloadBooks.books.entries.elementAt(Random.nextInt(downloadBooks.books.size))
+        coroutineScope.launch {
+            try{
+                val content = awaitForAll(urls)
 
-            Log.d("MyService ----->","downloading file ...${pair.key}")
-            handler.postDelayed({download()},1000)
+                content.forEach(){ book ->
+                    val words = book.split("\\s+".toRegex())
 
-            val words = pair.value.split("\\s+".toRegex())
-            val numByWord = words.groupingBy { it }.eachCount()
-            val mostCommon = numByWord.maxByOrNull { it.value }?.key ?: ""
+                    var wordsWOStopWords = words.filterNot { stopWords.contains(it.lowercase()) }
+                    
+                    val numByWord = wordsWOStopWords.groupingBy { it }.eachCount()
+                    val mostCommon = numByWord.maxByOrNull{ it.value }?.key?.trim(',', '.', '?', '"', '\'') ?: ""
 
+                    val bookInfo  = BookInfo(
+                        book.split("\n", ignoreCase = false, limit = 2)[0].substringAfter("The Project Gutenberg eBook of "),
+                        book.count { it == ' ' },
+                        book.count(),
+                        mostCommon)
 
-            val bookInfo  = BookInfo(
-                    pair.key,
-                    pair.value.count { it == ' ' },
-                    pair.value.count(),
-                    mostCommon)
-            Log.d("MyService ----->","${bookInfo.title}, ${bookInfo.wordCount}, ${bookInfo.charCount}, ${bookInfo.mostCommonWord} ")
+                    Log.d("MyService ----->","${bookInfo.title}, ${bookInfo.wordCount}, ${bookInfo.charCount}, ${bookInfo.mostCommonWord} ")
 
+                    Log.d("MyService ----->","Book Downloaded")
+                    val broadcastIntent = Intent("com.example.services.DATA_DOWNLOADED")
 
-            Log.d("MyService ----->","end")
-            val broadcastIntent = Intent("com.example.services.DATA_DOWNLOADED")
-
-
-
-            broadcastIntent.putExtra("TITLE", bookInfo.title)
-            broadcastIntent.putExtra("WORD_COUNT", bookInfo.wordCount)
-            broadcastIntent.putExtra("CHAR_COUNT", bookInfo.charCount)
-            broadcastIntent.putExtra("COMMON_WORD", bookInfo.mostCommonWord)
-            sendBroadcast(Intent(broadcastIntent))
-            stopSelf()
-
+                    broadcastIntent.putExtra("TITLE", bookInfo.title)
+                    broadcastIntent.putExtra("WORD_COUNT", bookInfo.wordCount)
+                    broadcastIntent.putExtra("CHAR_COUNT", bookInfo.charCount)
+                    broadcastIntent.putExtra("COMMON_WORD", bookInfo.mostCommonWord)
+                    sendBroadcast(Intent(broadcastIntent))
+                }
+            }catch (e : Exception){
+                Log.d("Download ---->" , "Exception: ${e.message}")
+            }finally {
+                stopSelf()
+            }
+        }
     }
 
     override fun onBind(p0: Intent?): IBinder? {
@@ -77,8 +111,9 @@ class MyService : Service() {
     }
 
     override fun onDestroy() {
+        coroutineScope.cancel()
         super.onDestroy()
-        handler.removeCallbacksAndMessages(null)
+        //handler.removeCallbacksAndMessages(null)
         Log.d("MyService ----->","onDestroy()")
     }
 }
